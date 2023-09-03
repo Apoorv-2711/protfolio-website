eaadb79f40aedbc1da7ba80f3f8c02e04eef2cba
Apoorv-2711 <apoorvjain162@gmail.com>
2023-09-04 04:37:00 +0530

    changes

--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -2,7 +2,7 @@ import { Skill } from "@/typings";
 
 export async function fetchSkills(): Promise<Skill[]> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/skill`, {
-    next: { revalidate: 3600 },
+    next: { revalidate: 1 },
   });
   const data = await res.json();
   const skills: Skill[] = data.skills;
